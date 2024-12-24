8877faaa6aa7fd32a505167517d82c102213a711
Giovanni Puccetti <g.puccetti92@gmail.com>
2024-12-24 13:27:44 +0100

    add loss to factory

--- src/open_clip/factory.py
+++ src/open_clip/factory.py
@@ -14,7 +14,7 @@ from .convert import convert_state_dict
 from .model import CLIP, CustomTextCLIP, convert_weights_to_lp, convert_to_custom_text_state_dict,\
     resize_pos_embed, get_cast_dtype, resize_text_pos_embed, set_model_preprocess_cfg
 from .coca_model import CoCa
-from .loss import ClipLoss, DistillClipLoss, CoCaLoss, SigLipLoss
+from .loss import ClipLoss, DistillClipLoss, CoCaLoss, SigLipLoss, DinoclipLoss
 from .pretrained import is_pretrained_cfg, get_pretrained_cfg, download_pretrained,\
     list_pretrained_tags_by_model, download_pretrained_from_hf
 from .transform import image_transform_v2, AugmentationCfg, PreprocessCfg, merge_preprocess_dict, merge_preprocess_kwargs
@@ -454,6 +454,15 @@ def create_loss(args):
             rank=args.rank,
             world_size=args.world_size,
             dist_impl=args.loss_dist_impl,  # siglip has multiple distributed implementations to choose from
+        )
+    elif args.dino_config_file is not None:
+        return DinoclipLoss(
+            local_loss=args.local_loss,
+            gather_with_grad=args.gather_with_grad,
+            cache_labels=True,
+            rank=args.rank,
+            world_size=args.world_size,
+            use_horovod=args.horovod,
         )
 
     return ClipLoss(
