6b8a6800fdbb2c5b76bf3f490d828caa92b63935
Giovanni Puccetti <g.puccetti92@gmail.com>
2024-12-24 13:30:26 +0100

    test dino clip and not normal clip

--- test_open_clip.sh
+++ test_open_clip.sh
@@ -1,11 +1,15 @@
-cd open_clip/src
-export CUDA_VISIBLE_DEVICES="2,3,4,7"
-torchrun --nproc_per_node 4 --master-addr 127.0.0.1 \
+cd /raid/homes/giovanni.puccetti/Repos/dinoclip/open_clip/src
+export CUDA_VISIBLE_DEVICES="0,6"
+torchrun --nproc_per_node 2 --master-addr 127.0.0.1 \
     --master-port 19500 \
     -m open_clip_train.main \
     --train-data '/raid/datasets/laion400m-data/{00000..00654}.tar' \
     --train-num-samples 10968539 \
     --dataset-type webdataset \
-    --batch-size 320 \
+    --batch-size 4 \
     --precision amp \
-    --workers 4
+    --model "ViT-L-16" \
+    --workers 2 \
+    --logs /raid/homes/giovanni.puccetti/Repos/dinoclip/logs \
+    --dino-config-file /raid/homes/giovanni.puccetti/Repos/dinoclip/dinov2/dinov2/configs/train/vitl16_short.yaml \
+    --dino-repo-path /raid/homes/giovanni.puccetti/Repos/dinoclip/dinov2/
