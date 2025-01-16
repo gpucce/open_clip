0e9f126ad5847219ea1ac70ef13475cc34136577
gpucce <g.puccetti92@gmail.com>
2025-01-16 09:40:42 +0100

    skip contrastive loss if loss weight is 0

--- src/open_clip/loss.py
+++ src/open_clip/loss.py
@@ -149,9 +149,10 @@ class DinoclipLoss(ClipLoss):
         assert image_features.shape[0] % text_features.shape[0] == 0
         image_features_splits = image_features.split(text_features.shape[0])
         out_dict = {"contrastive_loss":0}
-        for image_features in image_features_splits:
-            out_dict["contrastive_loss"] += super().forward(image_features, text_features, logit_scale, output_dict=False)
-        out_dict = {k: v * self.dino_contrastive_loss_weight for k, v in out_dict.items()}
+        if self.dino_contrastive_loss_weight > 0:
+            for image_features in image_features_splits:
+                out_dict["contrastive_loss"] += super().forward(image_features, text_features, logit_scale, output_dict=False)
+            out_dict = {k: v * self.dino_contrastive_loss_weight for k, v in out_dict.items()}
         dino_loss = {k: v * self.dino_loss_weight for k, v in dino_loss.items()}
         out_dict.update(dino_loss)
         return out_dict
